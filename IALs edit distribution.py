e388f2d69eb0b4979adcb44cb77c7527ef068550
Ever García <everalfonso.garcia01@estudiant.upf.edu>
2018-12-04 23:12:54 +0100

    Update IALs edit distribution.py
    we now also show the CDF of the data

--- IALs edit distribution.py
+++ IALs edit distribution.py
@@ -15,19 +15,44 @@ editcount using powerlaw.Fit().
 '''
 
 for l in langs_defs.IALs:
-    df = pd.read_csv('users coeditions/' + l + ' coeditions.csv')
+    df = pd.read_csv('../datasets/users coeditions/' + l + ' coeditions.csv')
     df = df['Contributions_in_' + l]
     df = df[~np.isnan(df)]
     data = np.array(df.values.tolist())
     
-    plt.title('PDF of edit distribution of ' + l + ' with alpha = ' 
-              + str(round(powerlaw.Fit(data, discrete = True).power_law.alpha, 
-                          2)))
+    plt.subplot(211)
+    
+    plt.title('PDF of edit distribution of ' + l)
     
     powerlaw.plot_pdf(data)
     
     plt.xlabel('Edits of distinct users')
     
     plt.ylabel('P(X = x)')
+    
+    plt.subplot(212)
+    
+    '''
+    Alpha refers to the theoretical slope of the function and sigma to the std
+    of the data.
+    '''
+    
+    alpha = round(powerlaw.Fit(data, discrete = True).power_law.alpha, 3)
+    sigma = round(powerlaw.Fit(data, discrete = True).power_law.sigma, 3)
+    
+    plt.title( 'CDF, similarly (α = ' + str(alpha) 
+               + ', σ = ' + str(sigma) + ')' )
+    
+    powerlaw.plot_cdf(data)
+    
+    plt.xlabel('Edits of distinct users')
+    
+    plt.ylabel('P(X = x)')
+    
+    plt.subplots_adjust(hspace = 0.8)
+    
+    plt.savefig( '../graphs/edits distributions/' +
+                 'PDF of edit distribution of ' + l + '.png', 
+                 dpi = 500)
     
     plt.show()
