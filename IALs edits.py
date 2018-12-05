e6094f1090fdf99e2f0def2df28915997a4c18ce
Ever García <everalfonso.garcia01@estudiant.upf.edu>
2018-12-05 21:28:49 +0100

    Update IALs edits.py
    show yticks

--- IALs edits.py
+++ IALs edits.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import langs_defs
 
-############################################
-# Users that edit in IAL and top languages # 
-############################################
+#########################################
+# Users' edits in IAL and top languages # 
+#########################################
 
 all_edits = []
 
@@ -16,13 +16,13 @@ every non-null user's editcount.
 '''
 
 for l in langs_defs.IALs:
-    df = pd.read_csv('users coeditions/' + l + ' coeditions.csv')
+    df = pd.read_csv('../datasets/users coeditions/' + l + ' coeditions.csv')
     df = df['Contributions_in_' + l]
     df = df[~np.isnan(df)]
     all_edits.append((l, df.values.tolist()))
         
 for tl in langs_defs.top_langs:
-    df2 = pd.read_csv('users edits in top langs/' + tl + ' edits.csv')
+    df2 = pd.read_csv('../datasets/users edits in top langs/' + tl + ' edits.csv')
     df2 = df2['Edits']
     all_edits.append((tl, df2.values.tolist()))
 
@@ -44,7 +44,14 @@ plt.boxplot( [ all_edits[i][1] for i in range(len(all_edits)) ],
              showfliers = False, 
              labels = [ all_edits[i][0] for i in range(len(all_edits)) ] )
 
+y_ticks = [ np.percentile(all_edits[i][1], percentile) 
+            for i in range(len(all_edits)) 
+            for percentile in [25,50,75] ]
+
 plt.title('Editors ordered by the ' + str(percentile) + '% percentile.')
 plt.xlabel('Language')
 plt.ylabel('Number of edits')
+plt.yticks(y_ticks)
+plt.grid(axis = 'y')
+plt.savefig('../graphs/Edits in IALs and top languages.png', dpi = 500)
 plt.show()
