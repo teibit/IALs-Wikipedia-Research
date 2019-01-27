2adae255900f94da4166c2c130d4873161a7b59a
Ever García <everalfonso.garcia01@estudiant.upf.edu>
2019-01-27 11:14:38 +0100

    Update IALs edit distribution.py
    added all graphs in a single plot for both distributions and fixed fit plotting

--- IALs edit distribution.py
+++ IALs edit distribution.py
@@ -20,39 +20,76 @@ for l in langs_defs.IALs:
     df = df[~np.isnan(df)]
     data = np.array(df.values.tolist())
     
-    plt.subplot(211)
+    f, (ax1, ax2) = plt.subplots(2, 1)
     
-    plt.title('PDF of edit distribution of ' + l)
+    ax1.set_title('PDF of edit distribution of ' + l)
     
-    powerlaw.plot_pdf(data)
+    ax1.set_ylabel('P(X = x)')
+    
+    fit = powerlaw.Fit(data, discrete=True)
+    
+    fit.power_law.plot_pdf(ax = ax1)
     
     plt.xlabel('Edits of distinct users')
-    
-    plt.ylabel('P(X = x)')
-    
-    plt.subplot(212)
     
     '''
     Alpha refers to the theoretical slope of the function and sigma to the std
     of the data.
     '''
     
-    alpha = round(powerlaw.Fit(data, discrete = True).power_law.alpha, 3)
-    sigma = round(powerlaw.Fit(data, discrete = True).power_law.sigma, 3)
+    alpha = round(fit.power_law.alpha, 3)
+    sigma = round(fit.power_law.sigma, 3)
     
-    plt.title( 'CDF, similarly (α = ' + str(alpha) 
-               + ', σ = ' + str(sigma) + ')' )
+    ax2.set_title( 'CDF, similarly (α = ' + str(alpha) 
+                   + ', σ = ' + str(sigma) + ')' )
     
     powerlaw.plot_cdf(data)
     
     plt.xlabel('Edits of distinct users')
     
-    plt.ylabel('P(X = x)')
-    
     plt.subplots_adjust(hspace = 0.8)
     
     plt.savefig( '../graphs/edits distributions/' +
-                 'PDF of edit distribution of ' + l + '.png', 
+                 'PDF and CDF of edit distribution of ' + l + '.png', 
                  dpi = 500)
     
     plt.show()
+
+
+for l in langs_defs.IALs:
+    df = pd.read_csv('../datasets/users coeditions/' + l + ' coeditions.csv')
+    df = df['Contributions_in_' + l]
+    df = df[~np.isnan(df)]
+    data = np.array(df.values.tolist())
+    
+    fit = powerlaw.Fit(data, discrete=True)
+    
+    fit.power_law.plot_pdf()
+    
+plt.xlabel('Edits of distinct users')
+
+plt.title('PDF of edit distribution of IALs users')
+
+plt.legend(langs_defs.IALs)
+
+plt.savefig( '../graphs/edits distributions/CDF of all.png', dpi = 500)
+    
+plt.show()
+
+for l in langs_defs.IALs:
+    df = pd.read_csv('../datasets/users coeditions/' + l + ' coeditions.csv')
+    df = df['Contributions_in_' + l]
+    df = df[~np.isnan(df)]
+    data = np.array(df.values.tolist())
+    
+    powerlaw.plot_cdf(data)
+    
+plt.xlabel('Edits of distinct users')
+
+plt.title('CDF of edit distribution of IALs users')
+
+plt.legend(langs_defs.IALs)
+
+plt.savefig( '../graphs/edits distributions/CDF of all.png', dpi = 500)
+    
+plt.show()
